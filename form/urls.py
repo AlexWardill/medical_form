b3bcc27950559be0f6e0754741503fbac9a363c1
Alex Wardill <alexander@wardill.net>
2022-07-21 23:47:28 +0100

    created response endpoint, creates new information object of form data

--- form/urls.py
+++ form/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.form, name='form')
+    path('', views.form, name='form'),
+    path('response', views.response, name='response')
 ]
--- form/views.py
+++ form/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from .models import Response
 
 def form(request):
     return render(request, 'form/form.html')
+
+def response(request):
+    if request.method== 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        address = address.POST['address']
+        city = city.POST['city']
+        postcode = postcode.POST['postcode']
+
+        information = Response.objects.create(
+            name=name,
+            email=email,
+            phone_number=phone_number,
+            address=address,
+            city=city,
+            postcode=postcode)
+
+    return render(request, 'form/response.html')
