cfe604656db5f9f835b3f0686210ddd5af7f2364
Alex Wardill <alexander@wardill.net>
2022-07-21 23:25:26 +0100

    created and migrated Response model

--- form/models.py
+++ form/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Response(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=10)
+
+
+    def __str__(self):
+        return self.name
